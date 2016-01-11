c8e2b7b8553861e168d776206b4fa2686ea7f405
Monpase <gsavlaev@mail.ru>
2016-01-11 21:22:35 +0300

    step2ggg

---
+++ PROJECT/DBSportingEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT
+{
+    class DBSportingEvent : DbContext
+    {
+        public DbSet<SportingEvent> Events {get; set;}
+    }
+}
